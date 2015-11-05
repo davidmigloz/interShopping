47e5e7f67d7917b386146456f0a755d624602e07
davidmigloz <me@davidmiguel.com>
2015-11-05 01:21:27 +0100

    General layout implemented (toolbar, fragment navigation drawer, tabs & floating action button)

---
+++ app/src/main/java/bendavid/is/intershopping/entities/ShoppingList.java
@@ -0,0 +1,19 @@
+package bendavid.is.intershopping.entities;
+
+import java.util.Random;
+
+public class ShoppingList {
+
+    private static final Random RANDOM = new Random();
+
+    public static final String[] shoppingLists = {
+            "25/11/2015",
+            "23/11/2015",
+            "21/11/2015",
+            "19/11/2015",
+            "17/11/2015",
+            "15/11/2015",
+            "11/11/2015",
+            "09/11/2015",
+    };
+}
---
+++ app/src/main/java/bendavid/is/intershopping/entities/Supermarket.java
@@ -0,0 +1,11 @@
+package bendavid.is.intershopping.entities;
+
+public class Supermarket {
+
+    public static final String[] supermarketsNames = {
+            "Auchan",
+            "Biedronka",
+            "Carrefour Express",
+            "Carrefour Market",
+    };
+}
